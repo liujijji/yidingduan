66c31c52acd0466887fc0956d04b994db7a1075b
刘吉祥 <1423806246@qq.com>
2022-11-07 19:19:56 +0800

    '最终完毕'

--- 11-7-移动端事件-刘吉祥/js/setRem.js
+++ 11-7-移动端事件-刘吉祥/js/setRem.js
@@ -1,9 +1,14 @@
+var flg = true;
+
 function setRem() {
     var ui_w = 375;
     var client_w = document.documentElement.clientWidth || document.body.clientWidth;
 
     var html = document.querySelector('html');
     html.style.fontSize = (client_w / ui_w) * 10 + 'px';
+    setTimeout(function() {
+        flg = true;
+    }, 1000)
 }
 
 window.onresize = setRem;
