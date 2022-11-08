2c25178e8aab7f7abb97f157459827e9946db1e8
刘吉祥 <1423806246@qq.com>
2022-11-08 21:20:11 +0800

    '最终'

---
+++ 11-8-响应式布局-刘吉祥/5-华为/js/setRem.js
@@ -0,0 +1,24 @@
+//   加节流 
+
+var flg = true
+
+function setRem() {
+    var ui_w = 375;
+
+    if (flg) {
+        flg = false
+            // 获取屏幕的宽度
+        var clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
+        clientWidth = clientWidth < 250 ? 250 : clientWidth;
+
+        // 通过js动态改变html根节点字体大小
+        var html_ = document.getElementsByTagName('body')[0];
+        html_.style.fontSize = (clientWidth / ui_w) * 10 + 'px';
+        setTimeout(function() {
+            flg = true
+        }, 100)
+    }
+}
+// window.onresize 浏览器被重置大小执行事件
+window.onresize = setRem;
+setRem()
