f6dc4c7e252080e7111978cc6431ccbf1bdd3fdc
刘吉祥 <1423806246@qq.com>
2022-11-08 10:49:32 +0800

    '修改js'

--- 11-7-移动端事件-刘吉祥/js/setRem.js
+++ 11-7-移动端事件-刘吉祥/js/setRem.js
@@ -3,12 +3,17 @@ var flg = true;
 function setRem() {
     var ui_w = 375;
     var client_w = document.documentElement.clientWidth || document.body.clientWidth;
-
+    client_w = client_w > 600 ? 600 : client_w;
+    client_w = client_w < 250 ? 250 : client_w;
     var html = document.querySelector('html');
     html.style.fontSize = (client_w / ui_w) * 10 + 'px';
     setTimeout(function() {
         flg = true;
     }, 1000)
 }
-
-window.onresize = setRem;
+var timer = null;
+window.onreset = function() {
+    clearTimeout(timer);
+    timer = setTimeout(setRem, 300)
+}
+window.onload = setRem;
