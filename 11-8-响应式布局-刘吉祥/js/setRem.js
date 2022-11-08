bfa6a11ca756566df607c923f1621edc3c7e7e33
刘吉祥 <1423806246@qq.com>
2022-11-08 15:01:47 +0800

    '哈哈哈'

---
+++ 11-8-响应式布局-刘吉祥/js/setRem.js
@@ -0,0 +1,14 @@
+function setRem() {
+    var ui_w = 375;
+    var client_w = document.documentElement.clientWidth || document.body.clientWidth;
+    client_w = client_w > 600 ? 600 : client_w;
+    client_w = client_w < 250 ? 250 : client_w;
+    var html = document.querySelector('html');
+    html.style.fontSize = (client_w / ui_w) * 10 + 'px';
+}
+var timer = null;
+window.onreset = function() {
+    clearTimeout(timer);
+    timer = setTimeout(setRem, 300)
+}
+window.onload = setRem;
